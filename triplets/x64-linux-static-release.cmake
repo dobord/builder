dd8b2ae2c0ee073daffdc2f8fc120c28760664e3
dobord <dobordx@gmail.com>
2026-09-16 20:19:47 +0300

    Fix missing CRT linkage in Linux static release triplet

--- triplets/x64-linux-static-release.cmake
+++ triplets/x64-linux-static-release.cmake
@@ -1,4 +1,7 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+# Match upstream x64-linux: vcpkg BUILD_INFO requires a nonempty CRT linkage.
+# This does not change static package libraries into shared libraries.
+set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_BUILD_TYPE release)
