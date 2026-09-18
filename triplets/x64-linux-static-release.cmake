53a4f5a8a6e37c307e72ddc679c09b2aeb8f20b4
dobord <dobordx@gmail.com>
2026-09-18 12:22:00 +0300

    build(cef): byte-align production Linux triplet with qualified vcpkg

--- triplets/x64-linux-static-release.cmake
+++ triplets/x64-linux-static-release.cmake
@@ -3,7 +3,6 @@ set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_BUILD_TYPE release)
-
 set(VCPKG_ENV_PASSTHROUGH_UNTRACKED
     CEF_STATIC_WORK
     CEF_STATIC_BUILD_TIMEOUT_SECONDS
@@ -11,9 +10,7 @@ set(VCPKG_ENV_PASSTHROUGH_UNTRACKED
     CEF_STATIC_PLATFORM_PREFIX
     CEF_STATIC_PLATFORM_SHA256
 )
-
 set(X_VCPKG_FORCE_VCPKG_X_LIBRARIES ON)
-
 if(PORT STREQUAL "cef-toolchain-runtime")
     find_program(_cef_runtime_cc NAMES gcc-14 REQUIRED)
     execute_process(COMMAND "${_cef_runtime_cc}" -dumpmachine
