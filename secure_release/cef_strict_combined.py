43f1ae64e417361f1edd84e373a0503d6ecb1ba6
dobord <dobordx@gmail.com>
2026-09-18 18:32:26 +0300

    ci: align strict combined chain with FreeRDP v10 registry

--- secure_release/cef_strict_combined.py
+++ secure_release/cef_strict_combined.py
@@ -21,7 +21,7 @@ import time
 from . import build_support, cef_build, cef_contract, crypto, safeio
 from . import cef_strict_iteration
 
-VCPKG = "6746bb33a98f92e43a931069bb5e2ee8e2db5b02"
+VCPKG = "03d570423f92d512c2abe9ddd27a91aa3a2620e5"
 UPSTREAM = "9e593bb18ea69cc5095e012465dcd675a822ed0d"
 CEF = "befa5c26c0c608165f27ac348e892305837fd614"
 LOCKFREECORO = "24038aed3a0be642adb60e71bd994ae8f0d90140"
